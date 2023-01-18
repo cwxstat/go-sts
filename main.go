36af6a5f016a6baf5780002ee8d9422d54dd33e5
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-01-17 20:38:08 -0500

    version 0.0.1

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	sessionV1 "github.com/aws/aws-sdk-go/aws/session"
+	stsV1 "github.com/aws/aws-sdk-go/service/sts"
+)
+
+type GetCallerIdentityAPI interface {
+	GetCallerIdentity(ctx context.Context,
+		params *sts.GetCallerIdentityInput,
+		optFns ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error)
+}
+
+type GetCallerIdentityV1API interface {
+	GetCallerIdentity(params *stsV1.GetCallerIdentityInput) (*stsV1.GetCallerIdentityOutput, error)
+}
+
+func GetCallerIdentity(ctx context.Context, client GetCallerIdentityAPI, params *sts.GetCallerIdentityInput) (*sts.GetCallerIdentityOutput, error) {
+	return client.GetCallerIdentity(ctx, params)
+}
+
+func GetCallerIdentityV1(client GetCallerIdentityV1API, params *stsV1.GetCallerIdentityInput) (*stsV1.GetCallerIdentityOutput, error) {
+	return client.GetCallerIdentity(params)
+}
+
+func V1() error {
+
+	sess := sessionV1.Must(sessionV1.NewSessionWithOptions(sessionV1.Options{
+		SharedConfigState: sessionV1.SharedConfigEnable,
+	}))
+	sess.Config.Region = aws.String("us-east-2")
+
+	svc := stsV1.New(sess)
+
+	result, err := GetCallerIdentityV1(svc, &stsV1.GetCallerIdentityInput{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\nVersion 1\n")
+	fmt.Printf("Account ID: %s\n", *result.Account)
+	fmt.Printf("User ID: %s\n", *result.UserId)
+	fmt.Printf("ARN: %s\n", *result.Arn)
+	fmt.Printf("\n")
+	return nil
+
+}
+
+func V2() error {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	client := sts.NewFromConfig(cfg)
+
+	result, err := GetCallerIdentity(context.TODO(), client, &sts.GetCallerIdentityInput{})
+	if err != nil {
+
+		return err
+	}
+	fmt.Printf("\nVersion 2\n")
+	fmt.Printf("Account ID: %s\n", *result.Account)
+	fmt.Printf("User ID: %s\n", *result.UserId)
+	fmt.Printf("ARN: %s\n", *result.Arn)
+	fmt.Printf("\n")
+
+	return nil
+}
+
+func main() {
+	err := V1()
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = V2()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
